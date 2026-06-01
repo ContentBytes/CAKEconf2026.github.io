c7f114d93a2c528243b59190f8f5c6690dc861d6
szaganek <szaganek@gmail.com>
2026-06-01 16:41:25 +0200

    refactor links

---
+++ src/assets/allUrls.js
@@ -0,0 +1,4 @@
+export const allUrls = {
+  callForSpeakersLink: "https://forms.gle/bHdcRyDTDuXhoeXL6",
+  getUpdatesLink: "https://dashboard.mailerlite.com/forms/1431354/152058250434446580/share",
+}
