1746626617d2e16dbca32493eb427770a4b8eeaf
szaganek <szaganek@gmail.com>
2026-06-02 09:18:35 +0200

    update link in footer

--- src/assets/allUrls.js
+++ src/assets/allUrls.js
@@ -1,4 +1,5 @@
 export const allUrls = {
   callForSpeakersLink: "https://forms.gle/bHdcRyDTDuXhoeXL6",
   getUpdatesLink: "https://dashboard.mailerlite.com/forms/1431354/152058250434446580/share",
+  contentBytes: "https://contentbytes.pl",
 }
