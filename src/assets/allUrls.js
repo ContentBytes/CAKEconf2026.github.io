c0f0fc940ae6a51ca3b25dd76f8e2ed3b08d195b
szaganek <szaganek@gmail.com>
2026-06-10 12:11:21 +0200

    update links

--- src/assets/allUrls.js
+++ src/assets/allUrls.js
@@ -2,4 +2,6 @@ export const allUrls = {
   callForSpeakersLink: "https://forms.gle/bHdcRyDTDuXhoeXL6",
   getUpdatesLink: "https://dashboard.mailerlite.com/forms/1431354/152058250434446580/share",
   contentBytes: "https://contentbytes.pl",
+  earlyBirdTicketLink: "https://www.naffy.io/content-bytes/cake-conf-early-bird",
+  ticketsGroupLink: "https://forms.gle/VpnrKur2S9AYyzuM7",
 }
