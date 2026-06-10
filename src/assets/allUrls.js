588cd711ac2fc02a539cd5c1371d93d956cbfaac
szaganek <szaganek@gmail.com>
2026-06-10 16:01:17 +0200

    Update group ticket link

--- src/assets/allUrls.js
+++ src/assets/allUrls.js
@@ -3,5 +3,5 @@ export const allUrls = {
   getUpdatesLink: "https://dashboard.mailerlite.com/forms/1431354/152058250434446580/share",
   contentBytes: "https://contentbytes.pl",
   earlyBirdTicketLink: "https://www.naffy.io/content-bytes/cake-conf-early-bird",
-  ticketsGroupLink: "https://forms.gle/VpnrKur2S9AYyzuM7",
+  ticketsGroupLink: "https://preview.mailerlite.io/forms/1431354/189807116261786681/share",
 }
