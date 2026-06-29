ad16ec1e749cf2fccdf01da313a75f86f181af93
szaganek <szaganek@gmail.com>
2026-06-29 09:05:29 +0200

    Prepare for regular tickets

--- src/assets/allUrls.js
+++ src/assets/allUrls.js
@@ -2,6 +2,5 @@ export const allUrls = {
   callForSpeakersLink: "https://forms.gle/bHdcRyDTDuXhoeXL6",
   getUpdatesLink: "https://dashboard.mailerlite.com/forms/1431354/152058250434446580/share",
   contentBytes: "https://contentbytes.pl",
-  earlyBirdTicketLink: "https://www.naffy.io/content-bytes/cake-conf-early-bird",
   ticketsGroupLink: "https://preview.mailerlite.io/forms/1431354/189807116261786681/share",
 }
