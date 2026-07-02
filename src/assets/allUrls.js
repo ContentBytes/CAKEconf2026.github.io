16f94bff1bbc9ea1524b08377b63be094254ccec
szaganek <szaganek@gmail.com>
2026-07-02 08:51:40 +0200

    Add regular tickets

--- src/assets/allUrls.js
+++ src/assets/allUrls.js
@@ -2,5 +2,6 @@ export const allUrls = {
   callForSpeakersLink: "https://forms.gle/bHdcRyDTDuXhoeXL6",
   getUpdatesLink: "https://dashboard.mailerlite.com/forms/1431354/152058250434446580/share",
   contentBytes: "https://contentbytes.pl",
-  ticketsGroupLink: "https://preview.mailerlite.io/forms/1431354/189807116261786681/share",
+  ticketsGroupLink: "https://forms.gle/w4n2KkmKmAMPbPM2A",
+  singleTicketLink: "https://buy.stripe.com/28EaEX20bcdr2145vo0Jq05",
 }
